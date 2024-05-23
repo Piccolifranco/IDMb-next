75d8fa806e5ac808e0c1547ef7fd6021f682d0c0
Piccolifranco <piccolifrancolucio@gmail.com>
2024-05-22 21:31:33 -0300

    finally...

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,16 @@ import {Suspense} from "react";
 const API_KEY = process.env.API_KEY
 
 export default function Home({searchParams}) {
-  <Suspense>
+  return (
+  <Suspense fallback={<p>cargando perro 🐕</p>}>
     <Movies searchParams={searchParams} />
   </Suspense>
-  
+  )
 }
 
 
 async function Movies({searchParams}) {
+  
   const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week` }?api_key=${API_KEY}&page=1`);
   const data = await res.json();
